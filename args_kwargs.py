f8955484d3bf89e535321be3203ae80edc9dd950
387358 <t387358@hotmail.com>
2015-09-05 16:23:46 +0800

    update args_kwargs.py

--- args_kwargs.py
+++ args_kwargs.py
@@ -8,4 +8,18 @@ def my_func( *args, **kwargs):
         print 'key:', key, 'val:', val
 
 
+print "my_func('a1', 'a2', 'a3', k1='v1', k2='v2', k3='v3')"
 my_func('a1', 'a2', 'a3', k1='v1', k2='v2', k3='v3')
+
+print
+print "l = ['a1', 'a2', 'a3']"
+print "d = {'k1':'v1', 'k2':'v2', 'k3':'v3'}"
+print
+print 'my_func(l)'
+l = ['a1', 'a2', 'a3']
+d = {'k1':'v1', 'k2':'v2', 'k3':'v3'}
+my_func(l)
+
+print
+print "my_func(*l, **d)"
+my_func(*l, **d)
