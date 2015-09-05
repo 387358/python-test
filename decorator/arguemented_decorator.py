d2616a975d3aa7328110b84045292e81f6d258c9
387358 <t387358@hotmail.com>
2015-09-05 23:32:51 +0800

    add class implement for arguemented decorator

--- decorator/arguemented_decorator.py
+++ decorator/arguemented_decorator.py
@@ -8,11 +8,12 @@ def who_say_hello(who):
         return say
     return who_say
 
-@who_say_hello('Tim')
+# hello = who_say_hello(who)(hello)
+@who_say_hello('John')
 def hello(name):
     return '"hello ' +  name + '"'
 
-print hello('Tracy')
+print hello('Jim')
 
 
 
@@ -26,6 +27,28 @@ arguements for the real decorator fucntion "who_say" to use.
 
 '''
 
+'''
+Or use the decorator class object is more clear for saperator init decorator and
+call the decorated function
+'''
+
+print '-------------------------'
+
+class who_say_hello2(object):
+    def __init__(self, who):
+        self.who = who
+
+    def __call__(self, f):
+        def say(name):
+            return self.who + ' say ' + f(name)
+        return say
+
+#hello2 = who_say_hello2(who)(hello2)
+@who_say_hello2('Tim')
+def hello2(name):
+    return '"hello ' +  name + '"'
+
+print hello2('Tracy')
 
 
 
